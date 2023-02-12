7b4fb71ba8b48aafe7c18f3229d1a8a058e75043
Lucas <lucasbarberisperez@gmail.com>
2023-02-12 22:38:36 +0100

    add tittle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,9 @@
     import Disciplinas from '../components/home/Disciplinas.svelte';
     import Contenido from '../components/home/Contenido.svelte';
 </script>
+<svelte:head>
+	<title>Ies azarquiel</title>
+</svelte:head>
 
 <div class="container-fluid">
        <Disciplinas/>
